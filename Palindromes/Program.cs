e47dbe9c2da9c4b75e4ad284b754cce02d8a8be9
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-12-08 10:12:17 +0200

    Numeric palindromes solution

---
+++ Palindromes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Palindromes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                if (IsPalindrome(i))
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"The sum of all numeric palindromes that are less than 10,000 is {sum}");
+        }
+
+        private static bool IsPalindrome(int num)
+        {
+            string s = num.ToString();
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i -1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
